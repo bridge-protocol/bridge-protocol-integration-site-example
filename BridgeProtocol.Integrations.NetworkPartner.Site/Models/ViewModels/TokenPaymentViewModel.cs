f707f378a1b3d8c6acb363cba758f6888cfd6a2e
Gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2019-03-11 13:22:08 -0400

    Updated to include blockchain payment from extension and allow transaction checking through the integration service

---
+++ BridgeProtocol.Integrations.NetworkPartner.Site/Models/ViewModels/TokenPaymentViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BridgeProtocol.Integrations.NetworkPartner.Site.Models.ViewModels
+{
+    public class TokenPaymentViewModel
+    {
+        public string BridgeProtocol_PaymentIdentifier { get; set; }
+        public int BridgeProtocol_PaymentAmount { get; set; }
+        public string BridgeProtocol_PaymentAddress { get; set; }
+        public string BridgeProtocol_PaymentTransactionId { get; set; }
+    }
+}
