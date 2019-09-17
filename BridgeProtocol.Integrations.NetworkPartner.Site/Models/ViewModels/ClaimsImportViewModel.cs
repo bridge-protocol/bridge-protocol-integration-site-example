d7134df51e2d0a5e74a648c7c3ebfaaec93ca272
Gubanotorious <6355333+gubanotorious@users.noreply.github.com>
2019-09-17 15:33:48 -0400

    Update to support new API and extension integration

---
+++ BridgeProtocol.Integrations.NetworkPartner.Site/Models/ViewModels/ClaimsImportViewModel.cs
@@ -0,0 +1,14 @@
+﻿using BridgeProtocol.Integrations.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BridgeProtocol.Integrations.NetworkPartner.Site.Models.ViewModels
+{
+    public class ClaimsImportViewModel
+    {
+        public string ClaimsImportRequest { get; set; }
+    }
+}
